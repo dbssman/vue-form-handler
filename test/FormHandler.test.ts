74024bd4bc5edbe6da15027d7384dbc05e8f01aa
dbssman <danielerequejo@gmail.com>
2022-12-20 23:38:58 +0100

    Basic testing for form handler component

--- test/FormHandler.test.ts
+++ test/FormHandler.test.ts
@@ -7,8 +7,38 @@ describe('FormHandler component testing', () => {
         expect(FormHandler).toBeTruthy();
 
         const wrapper = mount(FormHandler)
+        expect(wrapper.exists()).toBeTruthy()
+    })
+    it('Form handler scoped slot provides values and form state', () => {
+        expect(FormHandler).toBeTruthy();
 
-        console.log({wrapper})
-        expect(wrapper)
+        const wrapper = mount(FormHandler, {
+            slots: {
+                default: `<template #default="props">
+                    {{props}}
+                    </template>
+                `
+            }
+        })
+        expect(wrapper.html()).toContain('values')
+        expect(wrapper.html()).toContain('formState')
+    })
+    it('Form handler is correctly initialized', () => {
+        const initialValues = {
+            field1: 'something',
+            field2: 'some other thing'
+        }
+        const wrapper = mount(FormHandler, {
+            props: {
+                initialValues
+            },
+            slots: {
+                default: `<template #default="props">
+                    {{props}}
+                    </template>
+                `
+            }
+        })
+        expect(JSON.parse(wrapper.html())?.values).toStrictEqual(initialValues)
     })
 })
