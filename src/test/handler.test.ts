14b0f092a432fd81b69bfa4b1cdf3a74a5689420
Daniel Requejo <daniel.requejo@netcentric.biz>
2023-01-19 17:42:32 +0100

    📈 Improve testing & structure

--- src/test/handler.test.ts
+++ src/test/handler.test.ts
@@ -1,4 +1,4 @@
-import useFormHandler, { initialState } from '../src/useFormHandler';
+import useFormHandler, { initialState } from '../useFormHandler';
 import { expect, it, describe } from "vitest"
 
 const sleep = () => new Promise((resolve) => setTimeout(() => resolve(true), 50))
@@ -107,78 +107,5 @@ describe('Form handler testing', () => {
         });
         await setValue('field2', 'another thing')
         expect(modifiedValues()).toStrictEqual({ field2: 'another thing' })
-    })
-})
-
-describe('Register function testing', () => {
-    it('Registering a field', () => {
-        const { values, register } = useFormHandler();
-        const field = register('field')
-        expect(field.name).toBe('field')
-        expect(field.errors).toStrictEqual([])
-        expect(field.onBlur).toBeDefined()
-        expect(field.isDirty).toBeUndefined()
-        expect(field.isTouched).toBeUndefined()
-        expect(field.onClear).toBeDefined()
-        expect(field.onChange).toBeDefined()
-        expect(field.modelValue).toBe(null)
-        expect(field['onUpdate:modelValue']).toBeDefined()
-        expect(values.field).toBe(null)
-    })
-    it('Specified native field should have native handlers', () => {
-        const { register } = useFormHandler();
-        const field = register('field', { native: true })
-        expect(field.ref).toBeDefined()
-        expect(field.onChange).toBeDefined()
-    })
-    it('Specified custom field shouldn\'t have native handlers', () => {
-        const { register } = useFormHandler();
-        const field = register('field', { native: false })
-        expect(field.onChange).toBeUndefined()
-        expect(field.modelValue).toBeDefined()
-        expect(field['onUpdate:modelValue']).toBeDefined()
-    })
-    it('Input registered with details receives dirty and touched states', () => {
-        const { register } = useFormHandler();
-        const field = register('field', { withDetails: true })
-        expect(field.isDirty).toBeDefined()
-        expect(field.isTouched).toBeDefined()
-    })
-    it('Clearable inputs should have a bound clear handler', () => {
-        const { register } = useFormHandler();
-        const field = register('field', { clearable: true })
-        expect(field.onClear).toBeDefined()
-    })
-    it('Registering a field with default value', () => {
-        const { values, register } = useFormHandler();
-        register('field', { defaultValue: 'something' })
-        expect(values.field).toBe('something')
-    })
-    it('Registered validations work on update via handler', async () => {
-        const { values, register, formState } = useFormHandler();
-        const field = register('field', {
-            validations: {
-                error: (val) => val !== 'error' || 'Error with your field'
-            }
-        })
-        if (field['onUpdate:modelValue']) {
-            field['onUpdate:modelValue']('error')
-            await sleep()
-            expect(values.field).toBe('error')
-            expect(formState.isValid).toBeFalsy()
-        }
-    })
-    it('Registered validations work on update via setter', async () => {
-        const { values, register, formState, setValue, triggerValidation } = useFormHandler();
-        register('field', {
-            validations: {
-                error: (val) => val !== 'error' || 'Error with your field'
-            }
-        })
-        await setValue('field', 'error')
-        await triggerValidation('field')
-        expect(values.field).toBe('error')
-        await sleep()
-        expect(formState.isValid).toBeFalsy()
     })
 })
