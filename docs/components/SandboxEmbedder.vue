1f6371722ea7577c6bf52d429879b0df051766a4
dbssman <danielerequejo@gmail.com>
2023-02-04 21:40:49 +0100

    📄 Setup doc examples

---
+++ docs/components/SandboxEmbedder.vue
@@ -0,0 +1,43 @@
+<template>
+    <iframe
+        :src="`https://codesandbox.io/embed/github/dbssman/vue-form-handler/tree/master/examples/${example}?fontsize=12&hidenavigation=1&theme=dark`"
+        style="border:0;borderRadius:4;overflow:hidden" :style="styleObject"
+        :title="`dbssman/vue-form-handler: ${example}`"
+        allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"></iframe>
+</template>
+
+<script setup lang="ts">
+import { computed, onMounted, ref } from 'vue';
+
+defineProps({
+    example: {
+        type: String,
+        required: true,
+    },
+})
+
+const height = ref(0)
+const width = ref(0)
+const fixedContainerPadding = 64
+
+const styleObject = computed(() => ({
+    width: `${width.value - fixedContainerPadding}px`,
+    height: `${height.value - fixedContainerPadding}px`,
+}))
+
+const setDimensions = () => {
+    height.value = Number(document.querySelector('.VPContent')?.clientHeight)
+    width.value = Number(document.querySelector('.VPDoc')?.clientWidth)
+}
+
+onMounted(() => {
+    setDimensions()
+})
+
+window.addEventListener('resize', setDimensions)
+</script>
+
+<style>
+
+</style>
